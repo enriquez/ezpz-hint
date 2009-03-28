74936e38e9af6223f22c83c4c1d4d17e49a7b87d
Mike Enriquez <mike@enriquez.me>
2009-03-28 15:29:10 -0400

    initial import

---
+++ jquery.ezpz_hint.js
@@ -0,0 +1,56 @@
+// EZPZ Hint v1.0; Copyright (c) 2009 Mike Enriquez, http://theezpzway.com; Released under the MIT License
+(function($){
+	$.fn.ezpz_hint = function(options){
+		var defaults = {
+			hintClass: 'ezpz-hint',
+			hintName: 'ezpz_hint_dummy_input'
+		};
+		var settings = $.extend(defaults, options);
+		
+		return this.each(function(){
+			var hint;
+			var dummy_input;
+			
+			// grab the input's title attribute
+			text = $(this).attr('title');
+			
+			// create a dummy input and place it before the input
+			$('<input type="text" name="temp" value="" />').insertBefore($(this));
+			
+			// set the dummy input's attributes
+			hint = $(this).prev('input:first');
+			hint.attr('class', $(this).attr('class'));
+			hint.attr('size', $(this).attr('size'));
+			hint.attr('name', settings.hintName);
+			hint.attr('autocomplete', 'off');
+			hint.addClass(settings.hintClass);
+			hint.val(text);
+			
+			// hide the input
+			$(this).hide();
+			
+			// don't allow autocomplete (sorry, no remember password)
+			$(this).attr('autocomplete', 'off');
+			
+			// bind focus event on the dummy input to swap with the real input
+			hint.focus(function(){
+				dummy_input = $(this);
+				$(this).next('input:first').show();
+				$(this).next('input:first').focus();
+				$(this).next('input:first').unbind('blur').blur(function(){
+					if ($(this).val() == '') {
+						$(this).hide();
+						dummy_input.show();
+					}
+				});
+				$(this).hide();
+			});
+			
+			// remove the dummy inputs so that they don't get submitted
+			$('form').submit(function(){
+				$('.' + settings.hintClass).remove();
+			});
+		});
+		
+	};
+})(jQuery);
